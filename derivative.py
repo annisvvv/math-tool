c5b14151876a371862041824ce1144fd564c4200
annisvvv <anisnasri322@email.com>
2025-12-01 15:08:47 +0100

    .

--- derivative.py
+++ derivative.py
@@ -1,13 +1,27 @@
 import sympy as sp
+from sympy_commands import *
 
 def derivate():
-    # creating the variable for sympy
-    input_variable = input("give me the variable : ")
-    symp_variable = sp.symbols(input_variable)
 
-    # demand to the user to write the function
-    function = input("what is your function : ")
-    function_input = sp.sympify(function)
+    while True:
+        # creating the variable for sympy
+        input_variable = input("give me the variable (-h for help) : ")
+
+        if input_variable == "-h":
+            print_sympy_commands()
+        else:
+            symp_variable = sp.symbols(input_variable)
+            break
+
+    while True:
+        # demand to the user to write the function
+        function = input("what is your function (-h for help) : ")
+
+        if function == "-h":
+            print_sympy_commands()
+        else:
+            function_input = sp.sympify(function)
+            break
 
     # derivate the function
     derivative = sp.diff(function_input, symp_variable)
